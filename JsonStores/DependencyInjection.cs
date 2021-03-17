8eee50660d54129109036dd01557767f8a942401
Augusto César Bisognin <augustocb@gmail.com>
2021-03-16 23:23:33 -0300

    feat(core): dependency injection

---
+++ JsonStores/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace JsonStores
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddJsonStores([NotNull] this IServiceCollection services,
+            JsonStoreOptions options = default, ServiceLifetime serviceLifetime = ServiceLifetime.Scoped)
+        {
+            services.TryAdd(new ServiceDescriptor(typeof(JsonStoreOptions), options ?? new JsonStoreOptions()));
+
+            services.TryAdd(new ServiceDescriptor(typeof(IJsonStore<>), typeof(JsonStore<>), serviceLifetime));
+            services.TryAdd(
+                new ServiceDescriptor(typeof(IJsonRepository<,>), typeof(JsonRepository<,>), serviceLifetime));
+
+            return services;
+        }
+    }
+}
