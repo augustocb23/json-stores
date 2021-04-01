8ac783df0f4424f54015fd0afe335be01d62a966
Augusto César Bisognin <augustocb@gmail.com>
2021-03-31 21:58:59 -0300

    test(repository): RepositoryKey tests

---
+++ JsonStores.Tests/Repositories/RepositoryKey.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using JsonStores.Exceptions;
+using JsonStores.NamingStrategies;
+using JsonStores.Tests.Models;
+using Xunit;
+
+namespace JsonStores.Tests.Repositories
+{
+    public class RepositoryKey : IDisposable
+    {
+        private readonly string _path;
+
+        public RepositoryKey()
+        {
+            _path = Guid.NewGuid().ToString("N");
+        }
+
+        [Fact]
+        public void CreateRepositoryWithoutKey_Id()
+        {
+            var options = new JsonStoreOptions();
+            Assert.Throws<InvalidJsonRepositoryKeyException>(() =>
+                new JsonRepository<RepositoryTestModels.PersonWithoutId, int>(options));
+        }
+
+        [Fact]
+        public void CreateRepositoryWithWrongKeyType_Id()
+        {
+            var options = new JsonStoreOptions();
+            Assert.Throws<InvalidJsonRepositoryKeyException>(() => new JsonRepository<Person, string>(options));
+        }
+
+        [Fact]
+        public void CreateRepositoryWithWrongKeyType_Attribute()
+        {
+            var options = new JsonStoreOptions();
+            Assert.Throws<InvalidJsonRepositoryKeyException>(() =>
+                new JsonRepository<RepositoryTestModels.PersonIdWithAttribute, string>(options));
+        }
+
+        [Fact]
+        public async Task CreateRepositoryWithKey_Attribute()
+        {
+            JsonRepository<RepositoryTestModels.PersonIdWithAttribute, int> GetRepository() =>
+                new(new JsonStoreOptions {NamingStrategy = new StaticNamingStrategy(_path)});
+
+            var repository = GetRepository();
+
+            // add an item
+            const int key = 99;
+            var item = new RepositoryTestModels.PersonIdWithAttribute {Id = 1, Number = key, Name = "Number Nine-Nine"};
+            await repository.AddAsync(item);
+            await repository.SaveChangesAsync();
+
+            // use another repository (without any cache) to load the saved item
+            var newRepository = GetRepository();
+            var newItem = await newRepository.GetByIdAsync(key);
+
+            Assert.Equal(item, newItem);
+        }
+
+
+        [Fact]
+        public async Task CreateRepositoryWithLambda()
+        {
+            JsonRepository<RepositoryTestModels.PersonWithoutId, int> GetRepository() =>
+                new(new JsonStoreOptions {NamingStrategy = new StaticNamingStrategy(_path)},
+                    person => person.Number);
+
+            // add an item
+            var repository = GetRepository();
+            const int key = 99;
+            var item = new RepositoryTestModels.PersonWithoutId {Number = key, Name = "Number Nine-Nine"};
+            await repository.AddAsync(item);
+            await repository.SaveChangesAsync();
+
+            // use another repository (without any cache) to load the saved item
+            var newRepository = GetRepository();
+            var newItem = await newRepository.GetByIdAsync(key);
+
+            Assert.Equal(item, newItem);
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+
+            if (_path != null) File.Delete(_path);
+        }
+    }
+}
---
+++ JsonStores.Tests/Repositories/RepositoryTestModels.cs
@@ -0,0 +1,21 @@
+﻿using JsonStores.Annotations;
+
+namespace JsonStores.Tests.Repositories
+{
+    public static class RepositoryTestModels
+    {
+        public record PersonWithoutId
+        {
+            public int Number { get; set; }
+            public string Name { get; set; }
+        }
+
+        [IgnoreIdProperty]
+        public record PersonIdWithAttribute
+        {
+            [JsonRepositoryKey] public int Number { get; set; }
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
